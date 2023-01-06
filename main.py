de3165fc10858fee66b73c3a5bea95aa91115dcd
kkordik <99617240+Kkordik@users.noreply.github.com>
2023-01-07 00:10:02 +0100

    Remake structure

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 
-from config import BOT_TOKEN
+from config import *
 from main_interface.start_cmd import register_main_start_cmd
+
+from classes.database_classes import Database, UsersTable
+
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-if __name__ == '__main__':
+async def main(loop):
+    # Register database and pool
+    db = Database(HOST, USER, PASSWORD, NAME, PORT)
+    await db.make_pool(loop)
+
+    # Register user_table
+    user_tb = UsersTable(db)
+
+    # Register handlers
     register_main_start_cmd(dp)
-    executor.start_polling(dp)
+
+    await user_tb.insert_vals(user_id=1111)
+
+    await dp.start_polling()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
