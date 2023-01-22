3a677aa59699abd2ec745972faf4a8fd8c155be9
kkordik <99617240+Kkordik@users.noreply.github.com>
2023-01-22 22:28:39 +0100

    Creating keyboards file, start keyboard func

--- keyboards.py
+++ keyboards.py
@@ -2,7 +2,7 @@ from aiogram import types
 from texts import text
 
 
-def start_keyboard(language:str):
+def start_keyboard(language: str):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(types.KeyboardButton(text[language]["start_but1"]),
                  types.KeyboardButton(text[language]["start_but2"]))
