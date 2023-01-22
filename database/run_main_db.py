28e3dff5e871ea4628ecda16eb9923c9ab3a7a5e
kkordik <99617240+Kkordik@users.noreply.github.com>
2023-01-22 20:41:57 +0100

    Changing place of main database registration.

---
+++ database/run_main_db.py
@@ -0,0 +1,21 @@
+import asyncio
+from database.database_classes import Database, UsersTable, VideosTable, ProductsTable, PurchasesTable
+from config import HOST, USER, PASSWORD, NAME, PORT
+
+
+async def run_db(_loop, host, user, password, name, port):
+    db = Database(host, user, password, name, port)
+    await db.make_pool(_loop)
+    return db
+
+
+if __name__ == "database.run_main_db":
+    # Register database and pool
+    loop = asyncio.get_event_loop()
+    main_db = loop.run_until_complete(run_db(loop, HOST, USER, PASSWORD, NAME, PORT))
+
+    # Register main tables
+    user_tb = UsersTable(main_db)
+    video_tb = VideosTable(main_db)
+    prod_tb = ProductsTable(main_db)
+    purch_tb = PurchasesTable(main_db)
--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
 
 from config import *
 from main_interface.start_cmd import register_main_start_cmd
-
-from classes.database_classes import Database, UsersTable
 
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-async def main(loop):
-    # Register database and pool
-    db = Database(HOST, USER, PASSWORD, NAME, PORT)
-    await db.make_pool(loop)
-
-    # Register user_table
-    user_tb = UsersTable(db)
-
+async def main(_loop):
     # Register handlers
     register_main_start_cmd(dp)
 
