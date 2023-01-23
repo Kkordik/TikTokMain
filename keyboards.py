fb12e3d3b9a6efd93122bc6b40ccfd2a1d0609d4
kkordik <99617240+Kkordik@users.noreply.github.com>
2023-01-23 23:05:40 +0100

    Starting with product keyboard.

--- keyboards.py
+++ keyboards.py
@@ -7,3 +7,10 @@ def start_keyboard(language: str):
     keyboard.add(types.KeyboardButton(text[language]["start_but1"]),
                  types.KeyboardButton(text[language]["start_but2"]))
     return keyboard
+
+
+def shop_keyboard(language: str):
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(types.KeyboardButton(text[language]["start_but1"]),
+                 types.KeyboardButton(text[language]["start_but2"]))
+    return keyboard
